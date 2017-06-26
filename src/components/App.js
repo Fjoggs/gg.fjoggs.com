6ddb4e356881957842f0c71708675aa05e1a19b6
Vegard Fjogstad <fjoggis@gmail.com>
2017-06-26 14:59:53 +0200

    Begun work on pmp.org Added twitter bootstrap support

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../static/css/pmp2.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+          <div className="header"></div>
+          <div className="body"></div>
+          <div className="footer"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
