c0da3f3169b00125221aaf4d9dc4549931c4c7ac
Vegard Fjogstad <fjoggis@gmail.com>
2017-06-30 22:52:53 +0200

    Remove react-bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,17 @@ import '../static/css/pmp2.css';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-          <div className="header"></div>
-          <div className="body"></div>
-          <div className="footer"></div>
+      <div>
+        <div className="header"></div>
+        <div className="body">
+          <ul className="nav navbar-nav">
+            <li className="active">Home</li>
+            <li>News</li>
+            <li>Guides</li>
+            <li>Downloads</li>
+          </ul>
+        </div>
+        <div className="footer"></div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
